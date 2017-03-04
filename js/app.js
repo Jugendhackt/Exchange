1d05b036c03f865d9105485e8eeb6e65c5da168c
rhotep <andreas.pittrich@posteo.de>
2017-03-05 00:17:06 +0100

    as

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 	firebase.initializeApp(_jh_config.firebase)
 
 
@@ -10,31 +9,14 @@
 			profile_picture : imageUrl
 		})
 
-=======
-  firebase.initializeApp(_jh_config.firebase)
 
-  function writeUserData(userId, name, email, imageUrl) {
-	  firebase.database().ref('users/' + userId).set({
-	    username: name,
-	    email: email,
-	    profile_picture : imageUrl
-	  })
->>>>>>> cdbd296c315f33ee2cea9aa053ba9250da4939a0
 	}
 
 
 
-<<<<<<< HEAD
-var app = angular.module('jhvw', [
-=======
 var app = angular.module('Test', [
->>>>>>> cdbd296c315f33ee2cea9aa053ba9250da4939a0
-	'ngMaterial',
-	'firebase'
-])
 
 
-<<<<<<< HEAD
 app.controller("AppCtrl", [
 	'$scope',
 	'$firebaseArray',
@@ -63,17 +45,3 @@ app.controller("AppCtrl", [
 
 	}
 ]);
-=======
-app.controller("SampleCtrl", function($scope, $firebaseArray) {
-   var ref = firebase.database().ref().child("messages");
-  // create a synchronized array
-  // click on `index.html` above to see it used in the DOM!
-  $scope.messages = $firebaseArray(ref);
-
-  $scope.post = function(){
-  	$scope.messages.$add({message: $scope.message, from: $scope.username})
-  	$scope.message = ''
-  }
-
-});
->>>>>>> cdbd296c315f33ee2cea9aa053ba9250da4939a0
